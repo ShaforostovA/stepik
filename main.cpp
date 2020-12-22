2ecf48f279bfa40f2a966dabc8e8b8feafee7b85
BigSasochek <sashavisimsasha@gmail.com>
2020-12-22 21:19:04 +0500

    Добавил функцию "printVar"

--- main.cpp
+++ main.cpp
@@ -2,7 +2,13 @@
 
 using namespace std;
 
+size_t printVar(int &var);
+
 int main() {
     cout << "Hello, World!" << endl;
     return 0;
+}
+
+size_t printVar(int &var){
+    cout << var << endl;
 }
