bb079ec1552c1b9b36e2a5c9a761c5b9ca10baac
BigSasochek <sashavisimsasha@gmail.com>
2020-12-22 21:20:15 +0500

    Удалил лишние файлы :)

---
+++ cmake-build-debug/CMakeFiles/stepik.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stepik.dir/main.cpp.obj"
+  "libstepik.dll.a"
+  "stepik.exe"
+  "stepik.exe.manifest"
+  "stepik.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stepik.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
